83e37b7b3dbb4020a8ffd6e46daa30c21d727a38
oskarastrom <oskar.astrom@yahoo.se>
2023-11-25 22:06:47 +0100

    Update gyro.js

--- assets/js/gyro.js
+++ assets/js/gyro.js
@@ -1,7 +1,20 @@
 
 console.log("starting")
 
-document.addEventListener("click", () => console.log("hi"))
+document.addEventListener("click", () => {
+    console.log("hi")
+    
+    let gyroscope = new Gyroscope({ frequency: 60 });
+    gyroscope.addEventListener("reading", (e) => {
+      console.log(`Angular velocity along the X-axis ${gyroscope.x}`);
+      console.log(`Angular velocity along the Y-axis ${gyroscope.y}`);
+      console.log(`Angular velocity along the Z-axis ${gyroscope.z}`);
+    });
+    gyroscope.onerror = (e) => {
+        console.log(e)
+    }
+    gyroscope.start();
+})
 function handleOrientation(event) {
     console.log("triggered")
     const alpha = event.alpha;
@@ -13,3 +26,15 @@ function handleOrientation(event) {
     if (report_box != undefined) report_box.innerHTML = report
 }
 window.addEventListener('deviceorientation', handleOrientation);
+
+
+let gyroscope = new Gyroscope({ frequency: 60 });
+gyroscope.addEventListener("reading", (e) => {
+  console.log(`Angular velocity along the X-axis ${gyroscope.x}`);
+  console.log(`Angular velocity along the Y-axis ${gyroscope.y}`);
+  console.log(`Angular velocity along the Z-axis ${gyroscope.z}`);
+});
+gyroscope.onerror = (e) => {
+    console.log(e)
+}
+gyroscope.start();
