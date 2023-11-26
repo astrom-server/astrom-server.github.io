1269bc2f7764fa2745a39ec872c86197a50ab68e
oskarastrom <oskar.astrom@yahoo.se>
2023-11-26 19:46:58 +0100

    Update gyro.js

--- assets/js/gyro.js
+++ assets/js/gyro.js
@@ -7,9 +7,20 @@ function updateFieldIfNotNull(fieldName, value, precision=10){
   console.log(fieldName, value)
 }
 function handleOrientation(event) {
-    document.getElementsByTagName("body")[0].style.backgroundPositionX = Math.round(100*event.gamma/90) + "vw"
-    document.getElementsByTagName("body")[0].style.backgroundPositionY = Math.round(100*event.beta/90) + "vh"
+    //document.getElementsByTagName("body")[0].style.backgroundPositionX = Math.round(100*event.gamma/90) + "vw"
+    //document.getElementsByTagName("body")[0].style.backgroundPositionY = Math.round(100*event.beta/90) + "vh"
 }
+let x = 0;
+let y = 0;
+let v = 0.020;
+function handleMotionEvent(event) {
+    x += event.rotationRate.alpha*v;
+    y += event.rotationRate.gamma*v;
+
+    document.getElementsByTagName("body")[0].style.backgroundPositionX = Math.round(x) + "vw"
+    document.getElementsByTagName("body")[0].style.backgroundPositionY = Math.round(y) + "vh"
+}
+  
 
 document.addEventListener("click", function(e) {
     e.preventDefault();
@@ -24,10 +35,12 @@ document.addEventListener("click", function(e) {
     }
     
     if (is_running){
-      window.removeEventListener("deviceorientation", handleOrientation);
+      window.removeEventListener("deviceorientation", handleOrientation, true);
+      window.removeEventListener("devicemotion", handleMotionEvent, true);
       is_running = false;
     }else{
-      window.addEventListener("deviceorientation", handleOrientation);
+      window.addEventListener("deviceorientation", handleOrientation, true);
+      window.addEventListener("devicemotion", handleMotionEvent, true);
       is_running = true;
     }
     console.log(is_running)
