24e9720bcfe4ab25850a9e3b6a3939def8a8a30a
oskarastrom <oskar.astrom@yahoo.se>
2023-11-26 19:33:26 +0100

    gyro again?

--- assets/js/gyro.js
+++ assets/js/gyro.js
@@ -1,4 +1,7 @@
 
+console.log("starting")
+console.log(window.DeviceOrientationEvent)
+is_running = false
 
 function updateFieldIfNotNull(fieldName, value, precision=10){
   console.log(fieldName, value)
@@ -8,14 +11,24 @@ function handleOrientation(event) {
     document.getElementsByTagName("body")[0].style.backgroundPositionY = Math.round(100*event.beta/90) + "vh"
 }
 
-
-// Request permission for iOS 13+ devices
-if (
-  DeviceMotionEvent &&
-  typeof DeviceMotionEvent.requestPermission === "function"
-) {
-  DeviceMotionEvent.requestPermission();
-}
-
-console.log("Enabling Gyro")
-window.addEventListener("deviceorientation", handleOrientation);
+document.addEventListener("click", function(e) {
+    e.preventDefault();
+    console.log(is_running)
+    
+    // Request permission for iOS 13+ devices
+    if (
+      DeviceMotionEvent &&
+      typeof DeviceMotionEvent.requestPermission === "function"
+    ) {
+      DeviceMotionEvent.requestPermission();
+    }
+    
+    if (is_running){
+      window.removeEventListener("deviceorientation", handleOrientation);
+      is_running = false;
+    }else{
+      window.addEventListener("deviceorientation", handleOrientation);
+      is_running = true;
+    }
+    console.log(is_running)
+  })
