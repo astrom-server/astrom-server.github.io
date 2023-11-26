511147116bdb61a575ce151e275dc9893a6bf5c6
oskarastrom <oskar.astrom@yahoo.se>
2023-11-26 19:23:10 +0100

    Update spotify.js

--- assets/js/spotify.js
+++ assets/js/spotify.js
@@ -20,6 +20,7 @@ function authorize() {
             let response = JSON.parse(event.target.response)
             console.log(response)
             access_token = response.access_token
+            enable_gyro()
         } else if (event.target.status == 400) {
             console.error(event.target)
         }
@@ -117,6 +118,29 @@ function playSound(url) {
     current_audio.play();
 }
 
+function enable_gyro() {
+
+
+    function updateFieldIfNotNull(fieldName, value, precision=10){
+        console.log(fieldName, value)
+    }
+    function handleOrientation(event) {
+        document.getElementsByTagName("body")[0].style.backgroundPositionX = Math.round(100*event.gamma/90) + "vw"
+        document.getElementsByTagName("body")[0].style.backgroundPositionY = Math.round(100*event.beta/90) + "vh"
+    }
+    
+    
+    // Request permission for iOS 13+ devices
+    if (
+        DeviceMotionEvent &&
+        typeof DeviceMotionEvent.requestPermission === "function"
+    ) {
+        DeviceMotionEvent.requestPermission();
+    }
+    
+    console.log("Enabling Gyro")
+    window.addEventListener("deviceorientation", handleOrientation);
+}
 
 
 authorize()
