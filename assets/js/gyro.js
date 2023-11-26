815a39731aced1925dcd3787efe35cd2aa8aff03
oskarastrom <oskar.astrom@yahoo.se>
2023-11-26 13:14:27 +0100

    Gyro Space

--- assets/js/gyro.js
+++ assets/js/gyro.js
@@ -1,7 +1,4 @@
 
-console.log("starting")
-console.log(window.DeviceOrientationEvent)
-is_running = false
 
 function updateFieldIfNotNull(fieldName, value, precision=10){
   console.log(fieldName, value)
@@ -11,24 +8,14 @@ function handleOrientation(event) {
     document.getElementsByTagName("body")[0].style.backgroundPositionY = Math.round(100*event.beta/90) + "vh"
 }
 
-document.addEventListener("click", function(e) {
-    e.preventDefault();
-    console.log(is_running)
-    
-    // Request permission for iOS 13+ devices
-    if (
-      DeviceMotionEvent &&
-      typeof DeviceMotionEvent.requestPermission === "function"
-    ) {
-      DeviceMotionEvent.requestPermission();
-    }
-    
-    if (is_running){
-      window.removeEventListener("deviceorientation", handleOrientation);
-      is_running = false;
-    }else{
-      window.addEventListener("deviceorientation", handleOrientation);
-      is_running = true;
-    }
-    console.log(is_running)
-  })
+
+// Request permission for iOS 13+ devices
+if (
+  DeviceMotionEvent &&
+  typeof DeviceMotionEvent.requestPermission === "function"
+) {
+  DeviceMotionEvent.requestPermission();
+}
+
+console.log("Enabling Gyro")
+window.addEventListener("deviceorientation", handleOrientation);
